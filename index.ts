85b9f2f0600e237f809b1de2cecb47ddd5223597
Muhammad Asad <160310609+Asadkhan12334455@users.noreply.github.com>
2024-05-26 13:01:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,134 @@
+#!/usr/bin/env ts-node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+console.log(chalk.red.bold(`
+ $$$$\                                    
+$$  __$$\\                                   
+$$ /  \\__| $$$$$$\\  $$$$$$\\$$$$\\   $$$$$$\\  
+$$ |$$$$\\  \\____$$\\ $$  _$$  _$$\\ $$  __$$\\ 
+$$ |\\_$$ | $$$$$$$ |$$ / $$ / $$ |$$$$$$$$ |   
+$$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|                       
+\\$$$$$$  |\\$$$$$$$ |$$ | $$ | $$ |\\$$$$$$$\\ 
+ \\______/  \\_______|\\__| \\__| \\__| \\_______|
+`));
+console.log(chalk.bold.redBright("\n\t Welcome to Adventure Game \n\t"));
+class Player {
+    private _name: string;
+    private _fuel: number;
+    constructor(name: string) {
+        this._name = name;
+        this._fuel = 100;
+    }
+    get name() {
+        return this._name;
+    }
+    get fuel() {
+        return this._fuel;
+    }
+    decreaseFuel() {
+        this._fuel -= 20;
+        if (this._fuel < 0) {
+            this._fuel = 0;
+        }
+    }
+    increaseFuel() {
+        this._fuel = 100;
+    }
+}
+class Opponent {
+    private _name: string;
+    private _fuel: number;
+    constructor(name: string) {
+        this._name = name;
+        this._fuel = 100;
+    }
+    get name() {
+        return this._name;
+    }
+    get fuel() {
+        return this._fuel;
+    }
+    decreaseFuel() {
+        this._fuel -= 20;
+        if (this._fuel < 0) {
+            this._fuel = 0;
+        }
+    }
+}
+async function adventureGame() {
+    console.log(chalk.bold.yellow("Greetings from the First Battle Game!"));
+    const playerName = await getPlayerName();
+    const player = new Player(playerName);
+    const opponentName = await chooseOpponent();
+    const opponent = new Opponent(opponentName);
+    while (true) {
+        console.log(chalk.bold(`${player.name} VS ${opponent.name}`));
+        const action = await getPlayerAction();
+        switch (action) {
+            case 'Attack':
+                handleAttack(player, opponent);
+                if (opponent.fuel <= 0 || player.fuel <= 0) return;
+                break;
+                
+            case 'Drink Energy':
+                player.increaseFuel();
+                console.log(chalk.yellow(`Great! You drank an energy drink. Your fuel is now ${player.fuel}`));
+                break;
+                
+            case 'Run for life':
+                console.log(chalk.red.bold(`Player ${player.name} runs away!`));
+                return;
+        }
+    }
+}
+async function getPlayerName(): Promise<string> {
+    const { name } = await inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'Enter Player Name:'
+    });
+    return name;
+}
+async function chooseOpponent(): Promise<string> {
+    const { opponent } = await inquirer.prompt({
+        name: 'opponent',
+        type: 'list',
+        message: 'Select your opponent:',
+        choices: ['Dragon', 'Troll', 'Wizard']
+    });
+    return opponent;
+}
+async function getPlayerAction(): Promise<string> {
+    const { action } = await inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What do you want to do?',
+        choices: ['Attack', 'Drink Energy', 'Run for life']
+    });
+    return action;
+}
+function handleAttack(player: Player, opponent: Opponent) {
+    const playerWins = Math.random() < 0.5;
+    if (playerWins) {
+        opponent.decreaseFuel();
+        console.log(chalk.yellow(`${opponent.name}'s fuel is now ${opponent.fuel}`));
+        if (opponent.fuel <= 0) {
+            console.log(chalk.red.bold(`Player ${player.name} wins!`));
+        }
+    } else {
+        player.decreaseFuel();
+        console.log(chalk.yellow(`${player.name}'s fuel is now ${player.fuel}`));
+        if (player.fuel <= 0) {
+            console.log(chalk.red.bold(`Player ${player.name} loses!`));
+        }
+    }
+}
+async function main() {
+    try {
+        await adventureGame();
+        console.log(chalk.bold.yellow("Thanks for Playing!"));
+    } catch (error) {
+        console.error(chalk.red("An error  "))
+    }
+}
+main()
